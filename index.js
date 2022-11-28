9a1c6f4bd65558dd1361e1a4d41676d669c4785a
Daniel Acosta <daniel.acosta@mailonline.com>
2022-11-28 10:07:10 -0800

    added enterprise route

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get("/", function(req,res){
     res.type('html').send(getHTML)
 })
 
+app.get("/enterprisebuild/v4.7", function(req, res){
+    fs.readFile("public/enterprise.html", function(error, data){
+        res.writeHead(200, {"Content-Type":"text/html"})
+        res.end(data)
+    })
+})
+
 app.listen(process.env.PORT || 5000)
 
 const getHTML = "<html><body><h1>Hello There!</h1></body></html>"
